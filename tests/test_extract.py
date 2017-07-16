257d19079c5a617eb280003113f7ba071195778f
Adam <acf5118@rit.edu>
2017-07-15 21:58:11 -0400

    more robust test for extract

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,7 +1,12 @@
 import textract
+import os
 
-filename = input("Enter a filename: ")
-outfile = input ("Enter an output filename: ")
-text = textract.process(filename, method='pdftotext', layout=True)
-with open(outfile, "w") as text_file:
-    print(text.decode("utf-8"), file=text_file)
+
+input_dir = input("Enter a directory: ")
+for root, dirs, filenames in os.walk(input_dir):
+    for f in filenames:
+        if f.endswith(".pdf"):
+            filename = os.path.join(root, f)
+            text = textract.process(filename, method='pdftotext', layout=True)
+            with open(filename.replace(".pdf", ".txt"), "w") as text_file:
+                print(text.decode("utf-8"), file=text_file)
